f35a1b0120c2217db5813edb83f8425ec7755650
Anuj <122350668+anujbalak@users.noreply.github.com>
2024-12-10 20:37:18 +0530

    Add all initial files
    - Add jest
    
    - Add eslint
    
    - Add prettier
    
    - Add Webpack

---
+++ webpack.common.cjs
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'BattleShip',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'docs'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: "single",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                use: ['html-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                use: 'asset/resource',
+            },
+            {
+                test: /\.(png|jpeg|jpg|svg|gif)$/i,
+                use: 'asset/resource',
+            },
+        ],
+    },
+};
