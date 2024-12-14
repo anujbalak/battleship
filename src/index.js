ddaff82011cc226de9f8ac978dff97272e8ae0a8
Anuj <122350668+anujbalak@users.noreply.github.com>
2024-12-14 09:15:35 +0530

    Import player and computer gui module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 /* eslint-disable no-unused-vars */
 import { home } from "./app/home.mjs"
+import { playerGui } from "./app/playerGui.mjs"
+import { computerGui } from "./app/computerGui.mjs"
 import style from "./styles/style.css"
 
 home()
+playerGui()
+computerGui()
