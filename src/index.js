68842d0a7616acfb2bf715b784ecb72aaf56874d
Anuj <122350668+anujbalak@users.noreply.github.com>
2024-12-14 18:47:42 +0530

    Add battle module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
 import { home } from "./app/home.mjs"
 import { playerGui } from "./app/playerGui.mjs"
-import { computerGui } from "./app/computerGui.mjs"
+import { computerGui } from "./app/computerGui.mjs";
+import { battle } from "./app/battle.mjs"
 import style from "./styles/style.css"
 
 home()
 playerGui()
 computerGui()
+battle();
