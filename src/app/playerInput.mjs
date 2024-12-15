ab1e8d68a1ee3ffed99aa27b822c04185ff583e4
Anuj <122350668+anujbalak@users.noreply.github.com>
2024-12-15 15:48:34 +0530

    Change export function to playerRun

--- src/app/playerInput.mjs
+++ src/app/playerInput.mjs
@@ -1,14 +1,11 @@
 import { computerPlayer} from "./computerGui.mjs";
 import { refreshScore } from "./playerGui.mjs";
 
-function playerInput() {
-    let playerBoard = document.querySelectorAll('div.real-board span.cell')
-    let computerBoard = document.querySelectorAll('div.computer-board span.cell');
 
+function playerRun() {
     let isGameFinished = false;
-
+    let computerBoard = document.querySelectorAll('div.computer-board span.cell');
     computerBoard.forEach(square => {
-        
         square.addEventListener('click',() => {
             if (!isGameFinished) {
                 let recievedCords = square.getAttribute('value');
@@ -21,49 +18,47 @@ function playerInput() {
                     computerPlayer.gameboard.scoreBonus = 0
                 }
             }
-        }) 
-        
+        })   
     })
-
-    function hitShip(receiveCords) {
-        computerPlayer.gameboard.receiveAttack(receiveCords)
-        if (computerPlayer.gameboard.areAllShipSunk()) {
-            isGameFinished = true;
-        }
-    }
-
-    function styleHitSquare(square) {
-        let hit = computerPlayer.gameboard.isHit;
-        if (hit) {
-            square.innerText = '💣'
-            return square.classList.add('hit-ship');
-        }
-        square.innerText = '❎'
-        return square.classList.add('missed');
-    }
-
-    function styleSunkShip() {
-        let sunkShipsPosition = computerPlayer.gameboard.sunkShipsPosition;
-        let countSunkShips = sunkShipsPosition.length;
-        if (countSunkShips > computerPlayer.gameboard.totalSunkShips) {
-            let shipPosition = sunkShipsPosition[countSunkShips - 1];
-            shipPosition.forEach(cords => {
-                let square = document.querySelector(`div.computer-board span[value='${cords}']`);
-                square.classList.add('part-of-sunk-ship')
-                console.log(square);
-                square.innerText = '☠'
-            })
-            computerPlayer.gameboard.totalSunkShips += 1;
-        }
-    }
-
-    function makeArrayOfCords(cordsString) {
-        let cordsIntArr = cordsString.split(',');
-        let x = (cordsIntArr[0]);
-        let y = (cordsIntArr[1]);
-        return [x, y]
-    }
-
 }
 
-export { playerInput }
+function hitShip(receiveCords) {
+    computerPlayer.gameboard.receiveAttack(receiveCords)
+    if (computerPlayer.gameboard.areAllShipSunk()) {
+        isGameFinished = true;
+    }
+}
+
+function styleHitSquare(square) {
+    let hit = computerPlayer.gameboard.isHit;
+    if (hit) {
+        square.innerText = '💣'
+        return square.classList.add('hit-ship');
+    }
+    square.innerText = '❎'
+    return square.classList.add('missed');
+}
+
+function styleSunkShip() {
+    let sunkShipsPosition = computerPlayer.gameboard.sunkShipsPosition;
+    let countSunkShips = sunkShipsPosition.length;
+    if (countSunkShips > computerPlayer.gameboard.totalSunkShips) {
+        let shipPosition = sunkShipsPosition[countSunkShips - 1];
+        shipPosition.forEach(cords => {
+            let square = document.querySelector(`div.computer-board span[value='${cords}']`);
+            square.classList.add('part-of-sunk-ship')
+            console.log(square);
+            square.innerText = '☠'
+        })
+        computerPlayer.gameboard.totalSunkShips += 1;
+    }
+}
+
+function makeArrayOfCords(cordsString) {
+    let cordsIntArr = cordsString.split(',');
+    let x = (cordsIntArr[0]);
+    let y = (cordsIntArr[1]);
+    return [x, y]
+}
+
+export { playerRun }
