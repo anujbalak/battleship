2dc93c1dac18df07c488790922e9258bf70e3c9f
Anuj <122350668+anujbalak@users.noreply.github.com>
2024-12-24 08:47:27 +0530

    Create files to store the next possible moves by computer
    - Create a test file to test the moves
    - Move's x and y coordinates can not be more than 9 and less than 0

---
+++ src/module/possible-moves.mjs
@@ -0,0 +1,62 @@
+class Moves {
+    constructor() {
+        this.u = null
+        this.d = null
+        this.r = null;
+        this.l = null;
+    }
+
+    setUpMove(array) {
+        if (array.length > 2 || typeof(array) !== 'object') {
+            throw new Error('Array with length of 2 is required');
+        }
+        let x = array[0];
+        let oldY = array[1];
+        let newY = oldY + 1;
+        if (x > 9 || x < 0 || newY > 9 || newY < 0) {
+            return;
+        }
+        return this.u = [x, newY];
+    }
+
+    setDownMove(array) {
+        if (array.length > 2 || typeof(array) !== 'object') {
+            throw new Error('Array with length of 2 is required');
+        }
+        let x = array[0];
+        let oldY = array[1];
+        let newY = oldY - 1;
+        if (x > 9 || x < 0 || newY > 9 || newY < 0) {
+            return;
+        }
+        return this.d = [x, newY];
+    }
+
+    setRightMove(array) {
+        if (array.length > 2 || typeof(array) !== 'object') {
+            throw new Error('Array with length of 2 is required');
+        }
+        let oldX = array[0];
+        let y = array[1];
+        let newX = oldX + 1;
+        if (newX > 9 || newX < 0 || y > 9 || y < 0) {
+            return;
+        }
+        return this.r = [newX, y];
+    }
+
+    setLeftMove(array) {
+        if (array.length > 2 || typeof(array) !== 'object') {
+            throw new Error('Array with length of 2 is required');
+        }
+        let oldX = array[0];
+        let y = array[1];
+        let newX = oldX - 1;
+        if (newX > 9 || newX < 0 || y > 9 || y < 0) {
+            return;
+        }
+        return this.l = [newX, y];
+    }
+}
+
+export { Moves}
---
+++ src/test files/possible-moves.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+import { Moves } from "../module/possible-moves.mjs";
+
+
+let moves = new Moves();
+describe('Test moves', () => {
+    afterEach(() => {
+        console.log(moves)
+    })
+    test('Add up move', () => {
+        expect(moves.setUpMove([1, 3])).toEqual([1, 4])
+    })
+    test('Not a possible move', () =>{
+        expect(moves.setDownMove([4, 0])).toBe(undefined)
+    })
+    test('Add right move', () => {
+        expect(moves.setRightMove([3, 5])).toEqual([4, 5])
+    })
+    test('Add left move', () => {
+        expect(moves.setLeftMove([7, 8])).toEqual([6, 8])
+    })
+})
