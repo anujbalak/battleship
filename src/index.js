06a0f8378c14d65a5f7ddb24b62bf6726da7b48a
Anuj <122350668+anujbalak@users.noreply.github.com>
2024-12-29 14:34:29 +0530

    Adaptive style for diffrent mobiles and PCs

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { shipMenuUi } from "./app/ship-menu.mjs";
 import style from "./styles/style.css"
 import './styles/ship-menu.css'
 import './styles/highlights.css'
+import "./styles/adaptive_screen.css"
 home()
 playerGui()
 computerGui()
